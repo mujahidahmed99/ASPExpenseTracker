eeab0b573cab8da514ce0e52672ab9e3e239decf
Mujahid Ahmed <mujahidahmed99@hotmail.co.uk>
2021-09-25 22:13:28 +0100

    Expense Creation

---
+++ InAndOut/Migrations/20210920212142_changeExpenseCostType.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InAndOut.Migrations
+{
+    public partial class changeExpenseCostType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ExpenseName",
+                table: "Expenses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "ExpenseCost",
+                table: "Expenses",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ExpenseName",
+                table: "Expenses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ExpenseCost",
+                table: "Expenses",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
---
+++ InAndOut/Models/Expense.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Expense name")]
+        [Required]
+        public string ExpenseName { get; set; }
+        [DisplayName("Cost")]
+        [Required]
+        [Range(1, double.MaxValue, ErrorMessage ="Amount must be greater than 0!")]
+        public double ExpenseCost { get; set; }
+    }
+}
